379dc11a796cdb7fd6e5071fe22fb405f4f44a85
Rafael Pequino <o.rafaelpequino@gmail.com>
2024-09-08 23:05:45 -0300

    Initial commit

---
+++ src/components/CardCategoria.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import type ICategoria from '@/Interfaces/ICategoria';
+import type { PropType } from 'vue/dist/vue.js';
+
+export default {
+    props: {
+        categoria: { type: Object as PropType<ICategoria>, required: true }
+    }
+}
+</script>
+
+<template>
+    {{ categoria.nome }}
+    {{ categoria.imagem }}
+</template>
