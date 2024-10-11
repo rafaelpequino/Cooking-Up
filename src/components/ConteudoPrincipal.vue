b36544b242c6de245dd23d3d864a32549dc51031
Rafael Pequino <50996862811@azure.serpro.gov.br>
2024-10-11 11:26:03 -0300

    Adicionada lógica de alternância de telas

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,15 +1,19 @@
 <script lang="ts">
+import MostrarReceitas from './MostrarReceitas.vue';
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
 import SuaLista from './SuaLista.vue';
 import Tag from './Tag.vue';
 
+type Pagina = 'SelecionarIngredientes' | 'MostrarReceitas';
+
 export default {
     data() {
         return {
-            ingredientes: [] as string[]
+            ingredientes: [] as string[],
+            conteudo: 'SelecionarIngredientes' as Pagina
         }
     },
-    components: { SelecionarIngredientes, SuaLista, Tag },
+    components: { SelecionarIngredientes, SuaLista, Tag, MostrarReceitas },
     methods: {
         adicionarIngrediente(ingrediente: string) {
             this.ingredientes.push(ingrediente)
@@ -17,6 +21,9 @@ export default {
         removerIngrediente(ingrediente: string) {
             let index = this.ingredientes.indexOf(ingrediente)
             this.ingredientes.splice(index, 1)
+        },
+        navegar(pagina: Pagina) {
+            this.conteudo = pagina;
         }
     }
 }
@@ -24,11 +31,14 @@ export default {
 
 <template>
     <main class="conteudo-principal">
-        <SuaLista :ingredientes="ingredientes"/>
-        <SelecionarIngredientes 
-            @adicionar-ingrediente="adicionarIngrediente"
-            @remover-ingrediente="removerIngrediente"
-        />
+        <SuaLista :ingredientes="ingredientes" />
+        <KeepAlive include="SelecionarIngredientes">
+            <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
+                @adicionar-ingrediente="adicionarIngrediente" @remover-ingrediente="removerIngrediente"
+                @buscar-receitas="navegar('MostrarReceitas')" />
+            <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'"
+                @editar-receitas="navegar('SelecionarIngredientes')" />    
+        </KeepAlive>
     </main>
 </template>
 
